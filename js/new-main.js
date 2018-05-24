ca6af59d475b651945a9c4bd0298cc514b5603e4
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-05-24 17:20:04 +0300

    changes

---
+++ js/new-main.js
@@ -0,0 +1,29 @@
+;$(function(){
+
+    // $('.brands-slider').jcarousel({
+    //     vertical: true
+    // });
+    //
+    // $('.jcarousel-prev').jcarouselControl({
+    //     target: '-=1'
+    // });
+    //
+    // $('.jcarousel-next').jcarouselControl({
+    //     target: '+=1'
+    // });
+    //
+    // $(".brands-slider").royalSlider({
+    //     // options go here
+    //     // as an example, enable keyboard arrows nav
+    //     keyboardNavEnabled: true,
+    //     slidesOrientation: 'vertical',
+    //     minSlideOffset:10
+    // });
+
+    $(".verticalCarousel").verticalCarousel({
+        currentItem: 1,
+        showItems: 4
+
+    });
+
+});
