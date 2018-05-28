f5c72783eb702a8038a271dda59fca4cdc225018
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2018-05-28 12:48:40 +0300

    changes

--- js/new-main.js
+++ js/new-main.js
@@ -26,4 +26,42 @@
 
     });
 
+    var startMonth = 0,
+        endMonth = 0,
+        startCount = 5,
+        endCount = 8;
+
+    if(startCount < endCount){
+        startMonth = (startCount - 1) * 8.334;
+        endMonth = (12 - parseInt(endCount)) * 8.334;
+        console.log('startMonth: ' + startMonth);
+        console.log('endMonth: ' + endMonth);
+
+
+        $('.product-period__line').css({
+            left: startMonth + '%',
+            right: endMonth + '%'
+        });
+    }
+
+    $(document).ready(function() {
+        $(".fancybox").fancybox();
+    });
+
+
+
+    $(document).on('click', '.c-btn-expand', function(e){
+        e.preventDefault();
+        var self = $(this);
+        self.toggleClass('active');
+        var btnText = self.html();
+        if(btnText == 'свернуть'){
+            self.html('развернуть');
+        }else {
+            self.html('свернуть');
+        }
+        self.parents('.product-item__footer')
+            .siblings('.product-item__body').slideToggle(300)
+    })
+
 });
